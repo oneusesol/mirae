90c0cd9a78425e138ef5f31c6822c579ef23d7db
Leejehyuk <44721784+dlwpgur03@users.noreply.github.com>
2025-02-10 16:21:43 +0900

    메시지 입력

--- web/src/pages/AiCheck.js
+++ web/src/pages/AiCheck.js
@@ -8,7 +8,7 @@ function AiCheck() {
         <h2>피싱 문자 입력</h2>
         <textarea className="input-box" style={{ width: "100%", height: "300px", borderRadius: "10px" }} placeholder="검사할 문자를 입력하세요..."></textarea>
         <br />
-        <button className="check-btn" style={{ width: "100%", marginTop: "30px", height: "30px" }}>검사하기</button>
+        <button className="check-btn" style={{ width: "100%", marginTop: "30px", height: "30px", backgroundColor: "#003366", color: "white" }}>검사하기</button>
       </div>
 
       <div style={{ flex: 2 }}>
@@ -18,7 +18,7 @@ function AiCheck() {
           <textarea className="resale-box result-box" style={{ width: "100%", height: "300px", backgroundColor: "#f0f0f0", borderRadius: "10px" }} readOnly placeholder="추가 정보가 여기에 표시됩니다..."></textarea>
         </div>
         <br />
-        <button className="trend-btn" style={{ width: "100%", marginTop: "14px", height: "30px" }}>실시간 피싱 트렌드 보기</button>
+        <button className="trend-btn" style={{ width: "100%", marginTop: "14px", height: "30px", backgroundColor: "#003366", color: "white" }}>실시간 피싱 트렌드 보기</button>
       </div>
     </div>
   );
